6ebfe86eac5d7153c78df4c09d367f3bd17e3f07
evesquare <50450074+EveSquare@users.noreply.github.com>
2025-04-17 06:59:43 +0900

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by evesquare on 2025/04/17.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
